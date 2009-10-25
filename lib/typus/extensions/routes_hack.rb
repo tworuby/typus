daf6b5337d1054085e0a2e3045150fb5d6e374ec
Dieinzige <dieinzige@dieinziges-macbook-air.local>
2009-10-25 14:43:00 +0300

    If in hack routes  throug exception then we include at top priority action/id routes, wich override all rest routes , its so so so bad=(

--- lib/typus/extensions/routes_hack.rb
+++ lib/typus/extensions/routes_hack.rb
@@ -3,13 +3,13 @@ ActionController::Routing::Routes.draw do |map|
   begin
     ActionController::Routing::Routes.recognize_path '/admin/typus_users'
   rescue
-    map.connect ':controller/:action/:id'
+    #map.connect ':controller/:action/:id'
   end
 
   begin
     ActionController::Routing::Routes.recognize_path '/admin/typus_users.csv'
   rescue
-    map.connect ':controller.:format'
+    #map.connect ':controller.:format'
   end
 
 end
